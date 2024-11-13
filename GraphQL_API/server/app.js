f05484f614e43818de8dc33947366b0d35d79ec6
natewood2 <nathanwood.sfg@gmail.com>
2024-11-13 16:58:40 -0600

    Added all work for now

--- GraphQL_API/server/app.js
+++ GraphQL_API/server/app.js
@@ -1,6 +1,16 @@
 const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
 const schema = require('./schema/schema.js');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb+srv://<username>:<password>@<cluster>.mongodb.net/<dbname>?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+mongoose.connection.once('open', () => {
+  console.log('connected to database');
+});
 
 const app = express();
 
